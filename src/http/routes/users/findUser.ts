a80bc9b8006d6a9d59d3c12614af79da0139cf5b
Matheus Pimentel Leal <matheusp.eng@gmail.com>
2024-02-22 09:48:44 -0300

    :scroll: refactor api responses

--- src/http/routes/users/findUser.ts
+++ src/http/routes/users/findUser.ts
@@ -5,7 +5,7 @@ import z from 'zod'
 export async function findUser(app: FastifyInstance) {
   app.get('/users/:username', async (request, reply) => {
     const userInformation = z.object({
-      username: z.string().uuid()
+      username: z.string().min(6)
     })
 
     const { username } = userInformation.parse(request.params)
@@ -23,6 +23,12 @@ export async function findUser(app: FastifyInstance) {
       return reply.status(404).send({ message: 'User not found.' })
     }
 
-    return reply.send(user)
+    return reply.send({
+      user: {
+        username: user.username,
+        email: user.email,
+        lists: user.lists
+      }
+    })
   })
 }
--- src/http/routes/users/getUsers.ts
+++ src/http/routes/users/getUsers.ts
@@ -4,7 +4,13 @@ import { prisma } from '../../../lib/prisma'
 // TODO: Make the /users route only available to users that have already authenticated (use JWT)
 export async function getUsers(app: FastifyInstance) {
   app.get('/users', async (request, reply) => {
-    const users = await prisma.user.findMany()
+    const users = await prisma.user.findMany({
+      select: {
+        username: true,
+        email: true,
+        lists: true
+      }
+    })
 
     return reply.send({users: users})
   })
