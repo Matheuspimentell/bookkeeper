53e954e4e8d5fa1ef90bd0eb3b506ed075a602a4
Matheus Pimentel Leal <matheusp.eng@gmail.com>
2024-02-28 16:31:33 -0300

    :wrench: fixed jwt access token bug

--- src/utils/authenticateJwt.ts
+++ src/utils/authenticateJwt.ts
@@ -4,12 +4,16 @@ import { Prisma } from '@prisma/client'
 import { z } from 'zod'
 
 export async function authenticateAccess(request: FastifyRequest): Promise<Prisma.UserUpdateInput | undefined> {
-  const userParams = z.object({
-    id: z.string().uuid(),
-    username: z.string().min(6),
-    email: z.string().min(5),
+  const payloadParams = z.object({
+    data: z.object({
+      id: z.string().uuid(),
+      username: z.string().min(6),
+      email: z.string().min(5)
+    }),
+    iss: z.string(),
+    exp: z.number()
   })
-
+  
   const headers = request.headers['authorization']
   const token = headers && headers.split(' ')[1]
 
@@ -19,24 +23,34 @@ export async function authenticateAccess(request: FastifyRequest): Promise<Prism
   
   try {
     const { payload } = await jose.jwtVerify(token, secret)
+    
+    const { data, iss, exp } = payloadParams.parse(payload)
 
-    const user = userParams.parse(payload)
+    if(iss !== 'bookeeper-api') return undefined
 
-    return { 
-      id: user.id,
-      username: user.username,
-      email: user.email
-    }
+    return data ?? data
   } catch (error) {
     return undefined
   }
 }
 
-export async function authenticateRefresh(token: string): Promise<Object | undefined> {
+export async function authenticateRefresh(token: string): Promise<Object | undefined> {  
   if(!token) return undefined
+
+  const payloadParams = z.object({
+    data: z.object({
+      id: z.string().uuid(),
+      username: z.string().min(6),
+      email: z.string().min(5)
+    }),
+    iss: z.string(),
+  })
   
   const secret = new TextEncoder().encode(process.env.SECRET_REFRESH_KEY)
   const { payload }  = await jose.jwtVerify(token, secret)
+  const { data, iss } = payloadParams.parse(payload)
+  
+  if(iss !== 'bookeeper-api') return undefined
 
-  return payload ?? { user: payload }
+  return data ?? data
 }
